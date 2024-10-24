dafb6df3bd250b81390ae8686a59015ec53f410b
Parth0124 <abhangparth@gmail.com>
2024-10-25 01:03:31 +0530

    done with exception and logging

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,7 @@ LOG_FILE=f"{datetime.now().strftime('%m_%y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", 'Log_File')
 os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE_PATH)
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
